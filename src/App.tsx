a65b029b365e15486b480fd964a9ad7845fb283e
MiguelDuncan <165079717+MiguelDuncan@users.noreply.github.com>
2026-05-18 19:28:14 -0300

    Add Portfolio component and update imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
-import FAQ from './components/FAQ'; // Aqui avisamos o site que o FAQ existe
+import Portfolio from './components/Portfolio'; // Avisa o site que a galeria nova existe
+import FAQ from './components/FAQ';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -12,8 +13,9 @@ export default function App() {
       <Navbar />
       <Hero />
       <Services />
+      <Portfolio /> {/* Faz a sua nova galeria com fotos aparecer na tela! */}
       <About />
-      <FAQ /> {/* Aqui mandamos o FAQ aparecer logo após a seção "Sobre" */}
+      <FAQ />
       <Contact />
       <Footer />
     </div>
