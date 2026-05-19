cc2616f3dc5c587df8e18e70782498ef096d655f
MiguelDuncan <165079717+MiguelDuncan@users.noreply.github.com>
2026-05-19 00:23:17 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
-import Portfolio from './components/Portfolio'; // Ativa o arquivo de projetos
-import FAQ from './components/FAQ';             // Ativa o arquivo de perguntas
+// import Portfolio from './components/Portfolio'; 
+// import FAQ from './components/FAQ';             
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -13,9 +13,9 @@ export default function App() {
       <Navbar />
       <Hero />
       <Services />
-      <Portfolio /> {/* Força os projetos e o vídeo a aparecerem na tela */}
+      {/* <Portfolio /> */}
       <About />
-      <FAQ />       {/* Força as perguntas frequentes a aparecerem na tela */}
+      {/* <FAQ /> */}
       <Contact />
       <Footer />
     </div>
