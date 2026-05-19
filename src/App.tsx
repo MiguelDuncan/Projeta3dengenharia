884037cfa57dd04cc2529aecfc8eedd36f0caa81
MiguelDuncan <165079717+MiguelDuncan@users.noreply.github.com>
2026-05-18 22:54:59 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
-import Portfolio from './components/Portfolio'; // Avisa o site que a galeria nova existe
-import FAQ from './components/FAQ';
+import Portfolio from './components/Portfolio'; // Ativa o arquivo de projetos
+import FAQ from './components/FAQ';             // Ativa o arquivo de perguntas
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -13,9 +13,9 @@ export default function App() {
       <Navbar />
       <Hero />
       <Services />
-      <Portfolio /> {/* Faz a sua nova galeria com fotos aparecer na tela! */}
+      <Portfolio /> {/* Força os projetos e o vídeo a aparecerem na tela */}
       <About />
-      <FAQ />
+      <FAQ />       {/* Força as perguntas frequentes a aparecerem na tela */}
       <Contact />
       <Footer />
     </div>
